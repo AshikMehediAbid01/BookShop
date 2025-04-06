fd4c5b33c200f3a76ed83f4e7f5d3889f8ac93e1
Ashik Mehedi Abid01 <ashik.mehedi@brainstation-23.com>
2025-04-06 18:15:34 +0600

    Add project files.

---
+++ BookShop/Models/ProductTypes.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookShop.Models
+{
+    public class ProductTypes
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Product Type")]
+
+        public string ProductType { get; set; }
+         
+    }
+}
